2b45f253ac4a4aa38890a9c71aa93182099235d4
Awdhesh-Sahani01-hub <awdheshsahani6392789729@gmail.com>
2026-09-07 11:47:43 +0530

    Update production backend URL

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -96,7 +96,7 @@
 
      try {
 
-         const response = await fetch("https://portfolio-kv1q.onrender.com/send-message", {
+         const response = await fetch("https://portfolio-back-t8dd.onrender.com/send-message", {
              method: "POST",
 
              headers: {
